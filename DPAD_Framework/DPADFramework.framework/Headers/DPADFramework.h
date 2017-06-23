2a7be22ba61802fb11882b953096799e51343673
kdh <kdohyun21@naver.com>
2017-06-23 17:03:39 +0900

    매뉴얼 테스트 1차 업로드

---
+++ DPAD_Framework/DPADFramework.framework/Headers/DPADFramework.h
@@ -0,0 +1,18 @@
+//
+//  DPADFramework.h
+//  DPADFramework
+//
+//  Created by kdh on 2017. 6. 16..
+//  Copyright © 2017년 AresJoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DPADFramework.
+FOUNDATION_EXPORT double DPADFrameworkVersionNumber;
+
+//! Project version string for DPADFramework.
+FOUNDATION_EXPORT const unsigned char DPADFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DPADFramework/PublicHeader.h>
+#import <DPADFramework/DPManager.h>
